410406703fe990e254573303e9c4432c77ad6471
Chinbe02 <chinbe02@yahoo.co.jp>
2025-04-17 16:09:38 +0900

    Add Material-UI and button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div>
       <h1>ナオキの支出管理アプリ</h1>
-      <p>通信費と保険料の削減をサポート！</p>
+      <Button variant="contained">テストボタン</Button>
     </div>
   );
 }
